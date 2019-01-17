7a686f74b4a49a720660920937f23224e0f88280
Théo <45165017+theoflores@users.noreply.github.com>
2019-01-17 10:20:18 +0100

    Séparation des fichiers : HTML , CSS , JS

---
+++ TP_POPIN/script.js
@@ -0,0 +1,83 @@
+
+document.getElementById("valider").addEventListener("click", function (event) {
+    document.querySelector("span").append("Vous etes bien inscrit , vous allez donc recevoir un enfant gratuit par voie postale sous 5 jours ")
+    modal.style.display = "none";
+    event.preventDefault()
+});
+
+
+document.getElementById("email").addEventListener("keyup", function () {
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if ((document.getElementById("email").value === "") || (!filter.test(email.value))) {
+        document.getElementById('valider').disabled = true;
+
+    } else {
+        document.getElementById('valider').disabled = false;
+
+
+    }
+});
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// When the user press echap , close it
+window.onkeydown = function (event) {
+    if (event.keyCode == 27) {
+        modal.style.display = "none";
+    }
+};
+
+
+
+
+
+
+
+
+/*
+function showvalid() {
+    document.querySelector("span").append("Vous etes bien inscrit , vous allez donc recevoir un enfant gratuit par voie postale sous 5 jours ")
+}
+
+function success() {
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if ((document.getElementById("email").value === "") || (!filter.test(email.value))) {
+        document.getElementById('valider').disabled = true;
+
+    } else {
+        document.getElementById('valider').disabled = false;
+
+
+    }
+}
+
+*/
+
